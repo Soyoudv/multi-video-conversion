b77bd303b3f2dce434c3b5e80ece68912e0802fe
soyoudv <noname54765@gmail.com>
2026-02-12 12:28:14 +0100

    initial commit for .sh

---
+++ convert_all_to_mp4.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+set -e
+
+format_in="mkv"
+format_out="mp4"
+
+main (){
+
+    while getopts ": i: o: d:" opt; do
+        case ${opt} in
+            i) echo "Input format set to: $OPTARG"
+                format_in=$OPTARG
+            ;;
+            o) echo "Output format set to: $OPTARG"
+                format_out=$OPTARG
+            ;;
+            d) echo "Directory set to: $OPTARG"
+                if (cd "$OPTARG" 2>/dev/null); then
+                    echo "Directory set to: $OPTARG"
+                else
+                    echo "Directory does not exist: $OPTARG"
+                    exit 1
+                fi
+            ;;
+            ?)
+                echo "Invalid option: -$OPTARG" >&2
+                exit 1
+            ;;
+        esac
+    done
+
+
+
+
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+    cd "$DIR"
+    all_file_in=$(ls | grep ".${format_in}$" || true)
+    all_file_in=$(tr ' ' ';;' <<< "$all_file_in")
+    count_mkv_files=$(wc -w <<< "$all_file_in")
+    if [ "$count_mkv_files" -eq "0" ]; then
+        echo "No .${format_in} files found in the current directory."
+        exit 0
+    fi
+    echo "Found $count_mkv_files .${format_in} files. Starting conversion to .${format_out}..."
+
+    err=0
+
+    for file_in in $all_file_in; do
+
+        file_in=$(tr ';;' ' ' <<< "$file_in")
+        file_out="${file_in%.${format_in}}.${format_out}"
+
+        echo "Converting '$file_in' to '$file_out'..."
+        if (ffmpeg -i "$file_in" "$file_out" >> conversion.log 2>&1); then
+            echo "Successfully converted '$file_in' to '$file_out'."
+        else
+            err=$((err + 1))
+            echo "Failed to convert '$file_in'."
+        fi
+    done
+    if [ "$err" -ne 0 ]; then
+        echo "Conversion completed with $err errors."
+    else
+        echo "All files converted successfully without errors."
+    fi
+}
+main "$@"
