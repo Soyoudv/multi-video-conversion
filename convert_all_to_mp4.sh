8f3d250012a7ca946372b60cee9905e4014df1dc
soyoudv <noname54765@gmail.com>
2026-02-12 12:43:28 +0100

    mode silencieux et renommage de variables

--- convert_all_to_mp4.sh
+++ convert_all_to_mp4.sh
@@ -3,10 +3,11 @@ set -e
 
 format_in="mkv"
 format_out="mp4"
+output_log="conversion.log"
 
 main (){
 
-    while getopts ": i: o: d:" opt; do
+    while getopts ": i: o: d: s" opt; do
         case ${opt} in
             i) echo "Input format set to: $OPTARG"
                 format_in=$OPTARG
@@ -14,13 +15,17 @@ main (){
             o) echo "Output format set to: $OPTARG"
                 format_out=$OPTARG
             ;;
-            d) echo "Directory set to: $OPTARG"
+            d)
                 if (cd "$OPTARG" 2>/dev/null); then
                     echo "Directory set to: $OPTARG"
                 else
                     echo "Directory does not exist: $OPTARG"
                     exit 1
                 fi
+                cd "$OPTARG"
+            ;;
+            s) echo "Silent mode enabled. Suppressing output from ffmpeg."
+                 output_log="/dev/null"
             ;;
             ?)
                 echo "Invalid option: -$OPTARG" >&2
@@ -34,14 +39,15 @@ main (){
 
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
     cd "$DIR"
+    echo "Running in directory: $DIR"
     all_file_in=$(ls | grep ".${format_in}$" || true)
     all_file_in=$(tr ' ' ';;' <<< "$all_file_in")
-    count_mkv_files=$(wc -w <<< "$all_file_in")
-    if [ "$count_mkv_files" -eq "0" ]; then
+    count=$(wc -w <<< "$all_file_in")
+    if [ "$count" -eq "0" ]; then
         echo "No .${format_in} files found in the current directory."
         exit 0
     fi
-    echo "Found $count_mkv_files .${format_in} files. Starting conversion to .${format_out}..."
+    echo "Found $count .${format_in} files. Starting conversion to .${format_out}..."
 
     err=0
 
@@ -51,7 +57,7 @@ main (){
         file_out="${file_in%.${format_in}}.${format_out}"
 
         echo "Converting '$file_in' to '$file_out'..."
-        if (ffmpeg -i "$file_in" "$file_out" >> conversion.log 2>&1); then
+        if (ffmpeg -i "$file_in" "$file_out" >> $output_log 2>&1); then
             echo "Successfully converted '$file_in' to '$file_out'."
         else
             err=$((err + 1))
